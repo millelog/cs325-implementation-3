363885f6caa9a3c0fe8cc905ea8d2961921af955
sleek-turtle <reach.maddaloc@gmail.com>
2019-03-06 10:13:40 -0800

    fixed constraints, they were backwards. also fixed to integer soltuion

--- bagels/lp.py
+++ bagels/lp.py
@@ -4,19 +4,19 @@ import pulp as pulp
 
 my_lp_problem = pulp.LpProblem("Bagels n Muffins", pulp.LpMaximize)
 
-x = pulp.LpVariable('x', lowBound=0, cat='Continuous')
-y = pulp.LpVariable('y', lowBound=0, cat='Continuous')
+x = pulp.LpVariable('x', lowBound=0, cat='Integer')
+y = pulp.LpVariable('y', lowBound=0, cat='Integer')
 
-# y = Muffins
 # x = bagels
+# y = Muffins
 
 # Objective function
 my_lp_problem += 12 * x + 10 * y, "profit"
 
 # Constraints
-my_lp_problem += 5 * y + 4*x <= 50 # flour
-my_lp_problem += 2 * x +4 * y <= 30 #eggs
-my_lp_problem += 2 * y + 1 * x <= 20 #sugar
+my_lp_problem += 5 * x + 4 * y  <= 50 # flour
+my_lp_problem += 2 * x + 4 * y <= 30 #eggs
+my_lp_problem += 1 * x + 2 * y <= 20 #sugar
 
 
 status = my_lp_problem.solve()
